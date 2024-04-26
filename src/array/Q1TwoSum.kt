8590206e5504e62bd9df72787e3154245a4be6e4
Shyak Das <shayak.das007@gmail.com>
2024-04-27 01:04:49 +0530

    [Implementation]Two Sum implementation

--- src/array/Q1TwoSum.kt
+++ src/array/Q1TwoSum.kt
@@ -1,4 +1,35 @@
 package array
 
-class Q1TwoSum {
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    // First create a HashMap
+    var map = HashMap<Int, Int>()
+    // Iterate through the array
+    nums.forEachIndexed { index, value ->
+        // Check the diff here
+        // example diff = 9 -2 = 7
+        val diff = target - value
+        // Check diff is present or not
+        // example map does not have 7 key on it
+        if (map.containsKey(diff)) {
+            // If it contains, return the value of the key and index of the loop
+            return intArrayOf(map[diff]!!, index)
+        }
+        // If diff is not present on the map
+        // Put the value and index on the map
+        map[value] = index
+    }
+    return intArrayOf()
 }
